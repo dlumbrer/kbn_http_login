ad1011abb20e503df5d77b076b82d11fbb03d778
David Moreno Lumbreras <dmorenolumb@gmail.com>
2017-07-13 12:43:49 +0200

    Test http request with XMLHttpRequest
    Just testing the way to do a basic auth with Javascript using the
    library XMLHttpRequest.

--- public/app.js
+++ public/app.js
@@ -13,7 +13,6 @@ uiRoutes
   controllerAs: 'ctrl'
 })
 
-
 uiModules
 .get('app/http_login')
 .controller('httpLoginController', function ($scope, $http) {
@@ -21,7 +20,51 @@ uiModules
     this.indices = response.data;
   });*/
   $scope.login = function(){
-    console.log($scope.username, $scope.password)
+    console.log("Login: ", $scope.username, $scope.password)
+
+    var clientId = "bitergia";
+    var clientSecret = "bitergia";
+
+    var authorizationBasic = window.btoa(clientId + ':' + clientSecret);
+
+    /*var request = new XMLHttpRequest();
+    request.open('PUT', "https://localhost:9200/.kibana/login", true);
+    request.setRequestHeader('Content-Type', 'application/x-www-form-urlencoded; charset=UTF-8');
+    request.setRequestHeader('Authorization', 'Basic ' + authorizationBasic);
+    request.setRequestHeader('Accept', 'application/json');
+    request.send("username=bitergia&password=bitergia&grant_type=password");
+
+
+
+    request.onreadystatechange = function () {
+        if (request.readyState === 4) {
+           alert(request.responseText);
+        }
+    };*/
+
+    var xhr = new XMLHttpRequest();
+    /*xhr.open('POST', 'http://localhost:9200/_search', true);
+    xhr.withCredentials = true;
+    xhr.setRequestHeader('Content-Type', 'application/json');
+    xhr.setRequestHeader('Authorization', 'Basic ' + btoa('test:test'));
+    xhr.send('{"query":{"match_all":{}}}');*/
+
+    $.ajax
+    ({
+      type: "GET",
+      url: "https://localhost:5601/hrx/elasticsearch/_msearch",
+      dataType: 'json',
+      async: false,
+      data: '',
+      beforeSend: function (xhr){
+          xhr.setRequestHeader('Authorization', 'Basic ' + btoa('bitergia:bitergia'));
+      },
+      success: function (){
+          alert('Thanks for your comment!');
+      }
+    });
+
+
   }
 
 })
